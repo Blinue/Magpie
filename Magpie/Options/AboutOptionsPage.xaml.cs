ba1ca2b29fb9027213a218681708285cf2be21ed
Xu Liu <blinue@outlook.com>
2021-07-25 11:55:11 +0800

    完成关于页面

--- Magpie/Options/AboutOptionsPage.xaml.cs
+++ Magpie/Options/AboutOptionsPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Magpie {
     /// <summary>
@@ -20,6 +10,11 @@ namespace Magpie {
     public partial class AboutOptionsPage : Page {
         public AboutOptionsPage() {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e) {
+            _ = Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
