1b96d2a1660eb0b0a300aa544051cd8b41d61a9c
Xu Liu <blinue@outlook.com>
2021-07-27 21:30:59 +0800

    chore: 为c++项目添加版本资源

--- MODULE_Common/resource.h
+++ MODULE_Common/resource.h
@@ -1,15 +1,14 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
-// 供 Resource.rc 使用
+// 供 Version.rc 使用
 //
-#define IDB_RAVU_ZOOM_R3_WEIGHTS        106
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
