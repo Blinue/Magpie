b9ac21b5801932b8d62358f37bc918b6c838ea46
Xu Liu <blinue@outlook.com>
2021-07-27 23:27:20 +0800

    chore: 支持C++项目更新版本号

--- tools/VersionNumberUpdater/main.py
+++ tools/VersionNumberUpdater/main.py
@@ -1,9 +1,15 @@
 from os import path
 import glob
 import re
+import sys
 
-versionNumber = '0.6.0.0'
-versionNumberComma = versionNumber.replace('.', ',')
+assert len(sys.argv) == 2 and len(sys.argv[1]) > 0
+
+versionNumbers = list(map(lambda s: int(s), sys.argv[1].split('.')))
+versionNumbers.extend([0, 0, 0])
+
+version = '%d.%d.%d.%d' % tuple(versionNumbers[0:4])
+versionComma = version.replace('.', ',')
 
 rootDir = '..\\..'
 
@@ -15,8 +21,8 @@ for csProject in csProjects:
     with open(rootDir + '\\' + csProject + '\\Properties\\AssemblyInfo.cs', mode='r+', encoding='utf8') as f:
         src = f.read()
 
-        src = re.sub(r'AssemblyVersion\([^*]*?\)', 'AssemblyVersion(\"' + versionNumber + '\")', src)
-        src = re.sub(r'AssemblyFileVersion\([^*]*?\)', 'AssemblyFileVersion(\"' + versionNumber + '\")', src)
+        src = re.sub(r'AssemblyVersion\([^*]*?\)', 'AssemblyVersion(\"' + version + '\")', src)
+        src = re.sub(r'AssemblyFileVersion\([^*]*?\)', 'AssemblyFileVersion(\"' + version + '\")', src)
 
         f.seek(0)
         f.truncate()
@@ -26,9 +32,10 @@ for cppProject in cppProjects:
     with open(rootDir + '\\' + cppProject + '\\version.rc', mode='r+', encoding='utf8') as f:
         src = f.read()
 
-        #src = re.sub(r'FILEVERSION .*?\n', 'FILEVERSION ' + versionNumberComma + '\n', src)
-        #src = re.sub(r'PRODUCTVERSION .*?\n', 'PRODUCTVERSION ' + versionNumberComma + '\n', src)
-        src = re.sub(r'"FileVersion",(\b)*?".*?"\n', '"FileVersion", "' + versionNumber + '"\n', src)
+        src = re.sub(r'FILEVERSION .*?\n', 'FILEVERSION ' + versionComma + '\n', src)
+        src = re.sub(r'PRODUCTVERSION .*?\n', 'PRODUCTVERSION ' + versionComma + '\n', src)
+        src = re.sub(r'"FileVersion",[ ]*?".*?"\n', '"FileVersion", "' + version + '"\n', src)
+        src = re.sub(r'"ProductVersion",[ ]*?".*?"\n', '"ProductVersion", "' + version + '"\n', src)
 
         f.seek(0)
         f.truncate()
