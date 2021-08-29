08d027e476526424fafdf15e433a73c5de1846a5
Xu Liu <blinue@outlook.com>
2021-08-29 20:10:36 +0800

    fix: 优化打开缩放配置的逻辑 当找不到默认程序时，将尝试使用Notepad打开

--- Magpie/Options/ScaleOptionsPage.xaml.cs
+++ Magpie/Options/ScaleOptionsPage.xaml.cs
@@ -1,5 +1,6 @@
 using NLog;
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,8 +24,18 @@ namespace Magpie.Options {
 
 			try {
 				_ = Process.Start(psi);
+			} catch (Win32Exception ex) {
+				Logger.Warn(ex, "打开缩放配置失败，将尝试使用 Notepad 打开");
+
+				psi.FileName = "notepad";
+				psi.Arguments = App.SCALE_MODELS_JSON_PATH;
+				try {
+					_ = Process.Start(psi);
+				} catch (Exception ex1) {
+					Logger.Error(ex1, $"使用 Notepad 打开缩放配置失败\n\t缩放配置路径：{App.SCALE_MODELS_JSON_PATH}");
+				}
 			} catch (Exception ex) {
-				Logger.Error(ex, $"打开缩放配置失败\n\tSCALE_MODELS_JSON_PATH={App.SCALE_MODELS_JSON_PATH}");
+				Logger.Error(ex, $"打开缩放配置失败\n\t缩放配置路径：{App.SCALE_MODELS_JSON_PATH}");
 			}
 		}
 	}
