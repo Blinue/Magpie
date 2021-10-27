e1a0c42c7070a1af729aae85a4affa988074d1d1
Xu Liu <blinue@outlook.com>
2021-10-27 18:54:01 +0800

    feat: 支持将 mpv hook 的 TEXTURE 块转换为 DDS

---
+++ tools/MPVHookTextureParser/parse.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+python texture2Tiff.py %1 %2.tiff
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Error: texture2Tiff.py failed
+    PAUSE
+    EXIT 1
+)
+
+texconv -f R16G16B16A16_FLOAT -m 1 -fl 11.1 -y -nologo %2.tiff
+
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Error: Failed to convert .tiff to .dds
+    PASUE
+    EXIT 1
+)
+
+DEL %2.tiff
--- tools/MPVHookTextureParser/texture2Tiff.py
+++ tools/MPVHookTextureParser/texture2Tiff.py
@@ -1,10 +1,12 @@
-# TEXTURE.txt 格式：第一行为纹理的长和高，用空格隔开，第二行为16进制纹理数据
+# -*- coding: utf-8 -*-
 
 import struct
+import imageio
 import numpy as np
+import sys
 
 
-def resolve(in_file: str) -> np.ndarray:
+def resolve(in_file: str):
     with open(in_file) as f:
         header = f.readline()
         raw_data = f.read()
@@ -14,6 +16,10 @@ def resolve(in_file: str) -> np.ndarray:
     height = int(height)
 
     weights = struct.unpack("<%df" % (width * height * 4), bytes.fromhex(raw_data))
-    weights = np.array(weights, dtype=np.float)
+    return np.array(weights, dtype=np.float32).reshape((height, width, 4))
 
-    return weights.reshape((height, width, 4))
+
+IN_FILE = sys.argv[1]
+OUT_FILE = sys.argv[2]
+
+imageio.imwrite(OUT_FILE, resolve(IN_FILE))
