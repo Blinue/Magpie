a69f902713e4e211e480986229d112d0166ca618
Xu Liu <blinue@outlook.com>
2021-11-15 19:29:38 +0800

    chore: 为程序集添加版权声明

--- Magpie/Properties/AssemblyInfo.cs
+++ Magpie/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Magpie")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Liu Xu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
