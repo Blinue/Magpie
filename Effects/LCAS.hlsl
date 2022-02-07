541431939869287725331cca9d75b32a8dfe111c
Tzugimaa <tzugimaa.work@gmail.com>
2022-02-08 01:20:14 +0700

    Added edge checker

--- Effects/LCAS.hlsl
+++ Effects/LCAS.hlsl
@@ -32,10 +32,16 @@ float4 Pass1(float2 pos) {
 	float3 e = INPUT.Sample(sam, pos).rgb;
 	float3 f = INPUT.Sample(sam, pos + float2(inputPtX, 0)).rgb;
 	float3 h = INPUT.Sample(sam, pos + float2(0, inputPtY)).rgb;
-
+        
+	// Edge checker
+        float3 sum = (abs(e - b) - 0.5f) * 1.2 + 0.5f;
+	sum += (abs(e - f) - 0.5f) * 1.2 + 0.5f;
+	sum += (abs(e - h) - 0.5f) * 1.2 + 0.5f;
+	sum += (abs(e - d) - 0.5f) * 1.2 + 0.5f;
+	
 	// Soft min and max.
 	//    b
-	//	d e f
+	//  d e f
 	//    h
 
 	float3 mnRGB = min(min(min(d, e), min(f, b)), h);
@@ -49,5 +55,10 @@ float4 Pass1(float2 pos) {
 	//    w  
 	//  w 1 w
 	//    w   
-	return float4(((((b + d) + (f + h)) * wRGB + e) / (1.0 + 4.0 * wRGB)).rgb, 1);
+	// If is not edge
+	if(dot(0.0721f ,sum.g) <= 0.03)
+		return float4(((((b + d) + (f + h)) * wRGB + e) / (1.0 + 4.0 * wRGB)).rgb, 1);
+	else
+		return float4(((((b + d) + (f + h)) * wRGB * 1.5 + e) / (1.0 + 4.0 * wRGB)).rgb, 1);
+	// If is edge
 }
