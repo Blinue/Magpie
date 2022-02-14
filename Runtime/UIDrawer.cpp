7d0a355bf9f894286098bf1c70626527fb3e96da
刘旭 <xu.liu@bonsmile.com>
2022-02-14 11:05:04 +0800

    feat: 集成 ImGUI

---
+++ Runtime/UIDrawer.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "UIDrawer.h"
+#include "App.h"
+#include "DeviceResources.h"
+#include <imgui.h>
+#include "imgui/backends/imgui_impl_win32.h"
+#include "imgui/backends/imgui_impl_dx11.h"
+
+
+bool UIDrawer::Initialize() {
+	return false;
+}
+
+void UIDrawer::Draw() {
+
+}
---
+++ Runtime/UIDrawer.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "pch.h"
+
+
+class UIDrawer {
+public:
+	bool Initialize();
+
+	void Draw();
+};
