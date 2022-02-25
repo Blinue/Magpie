6eafd43be63e47453cb0b15dd90c950306667af0
Xu Liu <blinue@outlook.com>
2022-02-25 18:33:41 +0800

    perf: 优化 Bicubic 的性能

--- Effects/Bicubic.hlsl
+++ Effects/Bicubic.hlsl
@@ -54,21 +54,17 @@ float4 weight4(float x) {
 		weight(x - 1.0, B, C),
 		weight(x, B, C),
 		weight(x + 1.0, B, C)
-	);
-}
-
-float3 line_run(float ypos, float4 xpos, float4 linetaps) {
-	return INPUT.SampleLevel(sam, float2(xpos.r, ypos), 0).rgb * linetaps.r
-		+ INPUT.SampleLevel(sam, float2(xpos.g, ypos), 0).rgb * linetaps.g
-		+ INPUT.SampleLevel(sam, float2(xpos.b, ypos), 0).rgb * linetaps.b
-		+ INPUT.SampleLevel(sam, float2(xpos.a, ypos), 0).rgb * linetaps.a;
+		);
 }
 
 
 float4 Main(float2 pos) {
 	float2 inputSize = GetInputSize();
 	float2 inputPt = GetInputPt();
-	float2 f = frac(pos * inputSize + 0.5);
+	uint i, j;
+
+	pos *= inputSize;
+	float2 f = frac(pos + 0.5f);
 
 	float4 linetaps = weight4(1.0 - f.x);
 	float4 columntaps = weight4(1.0 - f.y);
@@ -77,15 +73,33 @@ float4 Main(float2 pos) {
 	linetaps /= linetaps.r + linetaps.g + linetaps.b + linetaps.a;
 	columntaps /= columntaps.r + columntaps.g + columntaps.b + columntaps.a;
 
-	// !!!改变当前坐标
-	pos -= (f + 1) * float2(inputPt.x, inputPt.y);
+	pos -= f + 0.5f;
 
-	float4 xpos = float4(pos.x, pos.x + inputPt.x, pos.x + 2 * inputPt.x, pos.x + 3 * inputPt.x);
+	float3 src[4][4];
 
-	// final sum and weight normalization
-	return float4(line_run(pos.y, xpos, linetaps) * columntaps.r
-		+ line_run(pos.y + inputPt.y, xpos, linetaps) * columntaps.g
-		+ line_run(pos.y + 2 * inputPt.y, xpos, linetaps) * columntaps.b
-		+ line_run(pos.y + 3 * inputPt.y, xpos, linetaps) * columntaps.a,
-		1);
+	[unroll]
+	for (i = 0; i < 4; i += 2) {
+		[unroll]
+		for (j = 0; j < 4; j += 2) {
+			float2 tpos = (pos + uint2(i, j)) * inputPt;
+			const float4 sr = INPUT.GatherRed(sam, tpos);
+			const float4 sg = INPUT.GatherGreen(sam, tpos);
+			const float4 sb = INPUT.GatherBlue(sam, tpos);
+
+			// w z
+			// x y
+			src[i][j] = float3(sr.w, sg.w, sb.w);
+			src[i][j + 1] = float3(sr.x, sg.x, sb.x);
+			src[i + 1][j] = float3(sr.z, sg.z, sb.z);
+			src[i + 1][j + 1] = float3(sr.y, sg.y, sb.y);
+		}
+	}
+
+	float3 result = float3(0, 0, 0);
+	[unroll]
+	for (i = 0; i < 4; ++i) {
+		result += (src[0][i] * linetaps.x + src[1][i] * linetaps.y + src[2][i] * linetaps.z + src[3][i] * linetaps.w) * columntaps[i];
+	}
+
+	return float4(result, 1);
 }
