8cc073c9d809de62825ef0738c7e2cd56ac1e032
Tzugimaa <tzugimaa.work@gmail.com>
2022-03-05 21:47:54 +0700

    Update LCAS.hlsl

--- Effects/LCAS.hlsl
+++ Effects/LCAS.hlsl
@@ -26,34 +26,34 @@ SamplerState sam;
 //!IN INPUT
 //!BLOCK_SIZE 8,8
 //!NUM_THREADS 64,1,1
-	
+
 void Pass1(uint2 blockStart, uint3 threadId) {
 	uint2 gxy = Rmp8x8(threadId.x) + blockStart;
+
 	if (!CheckViewport(gxy)) {
 		return;
 	}
 
-	float2 ppos = (gxy + 0.5f) * GetOutputPt();
+	float2 pos = (gxy + 0.5f) * GetOutputPt();
 
-        float2 inputPt = GetInputPt();
+      float2 inputPt = GetInputPt();
 	// fetch a 4 neighborhood pixels around the pixel 'e',
 	//	  b 
 	//	d(e)f
 	//	  h 
-	float3 b = INPUT.SampleLevel(sam, ppos + float2(0, -inputPt.y), 0).rgb;
-	float3 d = INPUT.SampleLevel(sam, ppos + float2(-inputPt.x, 0), 0).rgb;
-	float3 e = INPUT.SampleLevel(sam, ppos, 0).rgb;
-	float3 f = INPUT.SampleLevel(sam, ppos + float2(inputPt.x, 0), 0).rgb;
-	float3 h = INPUT.SampleLevel(sam, ppos + float2(0, inputPt.y), 0).rgb;
+	float3 b = INPUT.SampleLevel(sam, pos + float2(0, -inputPt.y), 0).rgb;
+	float3 d = INPUT.SampleLevel(sam, pos + float2(-inputPt.x, 0), 0).rgb;
+	float3 e = INPUT.SampleLevel(sam, pos, 0).rgb;
+	float3 f = INPUT.SampleLevel(sam, pos + float2(inputPt.x, 0), 0).rgb;
+	float3 h = INPUT.SampleLevel(sam, pos + float2(0, inputPt.y), 0).rgb;
 
 	// Edge checker.
-        float edge = length(abs(d - f) + abs(b - h));
+      float edge = length(abs(d - f) + abs(b - h));
 	
 	// Soft min and max.
 	//    b
 	//  d e f
 	//    h
-
 	float3 mnRGB = min(min(min(d, e), min(f, b)), h);
 
 	float3 mxRGB = max(max(max(d, e), max(f, b)), h);
@@ -69,5 +69,5 @@ void Pass1(uint2 blockStart, uint3 threadId) {
 	//  w 1 w
 	//    w   
 	float3 c = ((b + d + f + h) * wRGB + e) / (1.0 + 4.0 * wRGB);
-        WriteToOutput(gxy, (c + clamp(c, mnRGB, mxRGB) * 2) / 3);
+	WriteToOutput(gxy, (c + clamp(c, mnRGB, mxRGB) * 3) / 4);
 }
