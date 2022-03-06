053a60e2e0446752474dad0cf4d4e84262efeeb7
Xu Liu <blinue@outlook.com>
2022-03-06 17:43:28 +0800

    feat: 严重的日志也记录到调试器

--- Runtime/Logger.h
+++ Runtime/Logger.h
@@ -19,100 +19,51 @@ public:
 	}
 
 	void Info(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name()},
-			spdlog::level::info,
-			msg
-		);
+		_Log(spdlog::level::info, msg, location);
 	}
 
 	void Win32Info(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::info,
-			_MakeWin32ErrorMsg(msg)
-		);
+		_Log(spdlog::level::info, _MakeWin32ErrorMsg(msg), location);
 	}
 
 	void ComInfo(std::string_view msg, HRESULT hr, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::info,
-			_MakeComErrorMsg(msg, hr)
-		);
+		_Log(spdlog::level::info, _MakeComErrorMsg(msg, hr), location);
 	}
 
 	void Warn(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::warn,
-			msg
-		);
+		_Log(spdlog::level::warn, msg, location);
 	}
 
 	void Win32Warn(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::warn,
-			_MakeWin32ErrorMsg(msg)
-		);
+		_Log(spdlog::level::warn, _MakeWin32ErrorMsg(msg), location);
 	}
 
 	void ComWarn(std::string_view msg, HRESULT hr, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::warn,
-			_MakeComErrorMsg(msg, hr)
-		);
+		_Log(spdlog::level::warn, _MakeComErrorMsg(msg, hr), location);
 	}
 
 	void Error(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::err,
-			msg
-		);
+		_Log(spdlog::level::err, msg, location);
 	}
 
 	void Win32Error(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::err,
-			_MakeWin32ErrorMsg(msg)
-		);
+		_Log(spdlog::level::err, _MakeWin32ErrorMsg(msg), location);
 	}
 
 	void ComError(std::string_view msg, HRESULT hr, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(),
-			location.function_name() },
-			spdlog::level::err,
-			_MakeComErrorMsg(msg, hr)
-		);
+		_Log(spdlog::level::err, _MakeComErrorMsg(msg, hr), location);
 	}
 
 	void Critical(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::critical,
-			msg
-		);
+		_Log(spdlog::level::critical, msg, location);
 	}
 
 	void Win32Critical(std::string_view msg, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::critical,
-			_MakeWin32ErrorMsg(msg)
-		);
+		_Log(spdlog::level::critical, _MakeWin32ErrorMsg(msg), location);
 	}
 
 	void ComCritical(std::string_view msg, HRESULT hr, const std::source_location& location = std::source_location::current()) {
-		_logger->log(
-			spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
-			spdlog::level::critical,
-			_MakeComErrorMsg(msg, hr)
-		);
+		_Log(spdlog::level::critical, _MakeComErrorMsg(msg, hr), location);
 	}
 
 private:
@@ -123,6 +74,8 @@ private:
 	static std::string _MakeComErrorMsg(std::string_view msg, HRESULT hr) {
 		return fmt::sprintf("%s\n\tHRESULT：0x%X", msg, hr);
 	}
+
+	void _Log(spdlog::level::level_enum logLevel, std::string_view msg, const std::source_location& location);
 
 	std::shared_ptr<spdlog::logger> _logger;
 };
