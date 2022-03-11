6891c9380015cfaf0a12b0df706afa42644aa337
刘旭 <xu.liu@bonsmile.com>
2022-03-11 17:28:14 +0800

    chore: 删除项目文件 所有项目将移到 src 文件夹中

--- Runtime/TextureLoader.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-
-
-class TextureLoader {
-public:
-	static winrt::com_ptr<ID3D11Texture2D> Load(const wchar_t* fileName);
-};
--- Runtime/imgui_impl_magpie.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-
-
-bool ImGui_ImplMagpie_Init();
-void ImGui_ImplMagpie_Shutdown();
-void ImGui_ImplMagpie_NewFrame();
-
-LRESULT ImGui_ImplMagpie_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
