f962e72de4ef5ddfed78882aeeccaf0331eb0293
刘旭 <blinue@outlook.com>
2022-05-17 10:22:34 +0800

    UI: 添加若干样式文件 取自 PowerToys

---
+++ src/Magpie.App/Setting.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "Setting.h"
+#if __has_include("Controls.Setting.g.cpp")
+#include "Controls.Setting.g.cpp"
+#endif
+
+namespace winrt::Magpie::App::Controls::implementation
+{
+    
+}
---
+++ src/Magpie.App/Setting.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Controls.Setting.g.h"
+
+namespace winrt::Magpie::App::Controls::implementation
+{
+    struct Setting : SettingT<Setting>
+    {
+        Setting() = default;
+    };
+}
+
+namespace winrt::Magpie::App::Controls::factory_implementation
+{
+    struct Setting : SettingT<Setting, implementation::Setting>
+    {
+    };
+}
