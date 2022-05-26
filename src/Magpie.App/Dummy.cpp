b265b9b124f8e2898225c379b9e70af4abb08d04
刘旭 <blinue@outlook.com>
2022-05-26 15:55:46 +0800

    fix: 添加空的模板控件以解决页面切换的动画错误

---
+++ src/Magpie.App/Dummy.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "Dummy.h"
+#if __has_include("Controls.Dummy.g.cpp")
+#include "Controls.Dummy.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Automation::Peers;
+
+
+namespace winrt::Magpie::App::Controls::implementation {
+
+Dummy::Dummy() {
+	DefaultStyleKey(box_value(name_of<Magpie::App::Controls::Dummy>()));
+}
+
+}
---
+++ src/Magpie.App/Dummy.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Controls.Dummy.g.h"
+
+namespace winrt::Magpie::App::Controls::implementation
+{
+    struct Dummy : DummyT<Dummy>
+    {
+        Dummy();
+    };
+}
+
+namespace winrt::Magpie::App::Controls::factory_implementation
+{
+    struct Dummy : DummyT<Dummy, implementation::Dummy>
+    {
+    };
+}
