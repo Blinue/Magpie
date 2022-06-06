d54bd9962f2881ed385b01deccccc5eeb2f1cd19
刘旭 <blinue@outlook.com>
2022-06-06 19:39:53 +0800

    feat: 添加 ShortcutDialogContent

---
+++ src/App/ShortcutDialogContent.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "ShortcutDialogContent.h"
+#if __has_include("ShortcutDialogContent.g.cpp")
+#include "ShortcutDialogContent.g.cpp"
+#endif
+
+
+using namespace winrt;
+
+
+namespace winrt::Magpie::implementation {
+
+ShortcutDialogContent::ShortcutDialogContent() {
+    InitializeComponent();
+}
+
+void ShortcutDialogContent::IsError(bool value) {
+}
+
+bool ShortcutDialogContent::IsError() const {
+    return false;
+}
+
+void ShortcutDialogContent::Keys(const IVector<IInspectable>& value) {
+}
+
+IVector<IInspectable> ShortcutDialogContent::Keys() const {
+    return IVector<IInspectable>();
+}
+
+}
---
+++ src/App/ShortcutDialogContent.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "winrt/Windows.UI.Xaml.h"
+#include "winrt/Windows.UI.Xaml.Markup.h"
+#include "winrt/Windows.UI.Xaml.Interop.h"
+#include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
+#include "ShortcutDialogContent.g.h"
+
+
+namespace winrt::Magpie::implementation {
+
+struct ShortcutDialogContent : ShortcutDialogContentT<ShortcutDialogContent> {
+    ShortcutDialogContent();
+
+    void IsError(bool value);
+    bool IsError() const;
+
+    void Keys(const IVector<IInspectable>& value);
+    IVector<IInspectable> Keys() const;
+};
+
+}
+
+namespace winrt::Magpie::factory_implementation {
+
+struct ShortcutDialogContent : ShortcutDialogContentT<ShortcutDialogContent, implementation::ShortcutDialogContent> {
+};
+
+}
