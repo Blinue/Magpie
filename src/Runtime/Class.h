8609d68523d96374bee91389ff78e18cc3e9a33e
刘旭 <blinue@outlook.com>
2022-06-14 16:27:01 +0800

    feat: 添加 Runtime 项目用于实现核心功能

---
+++ src/Runtime/Class.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::Runtime::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Runtime::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
---
+++ src/Runtime/pch.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+
+#include "CommonPCH.h"
