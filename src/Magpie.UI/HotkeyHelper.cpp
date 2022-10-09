aee5e0f6a1317e47627d6f72b587a77ad6f88e1f
刘旭 <blinue@outlook.com>
2022-10-09 23:23:52 +0800

    fix: 修复警告

--- src/Magpie.UI/HotkeyHelper.cpp
+++ src/Magpie.UI/HotkeyHelper.cpp
@@ -28,54 +28,52 @@ static const phmap::flat_hash_set<DWORD>& GetValidKeyCodes() {
 		phmap::flat_hash_set<DWORD> keyCodes;
 		keyCodes.reserve(99);
 
-		if (keyCodes.empty()) {
-			// 字母
-			for (DWORD i = 'A'; i <= 'Z'; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// 数字（顶部）
-			for (DWORD i = '0'; i <= '9'; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// 数字（小键盘）
-			for (DWORD i = VK_NUMPAD0; i <= VK_NUMPAD9; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// F1~F24
-			for (DWORD i = VK_F1; i <= VK_F24; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// 空格、Page Up/Down、End、Home、方向键
-			for (DWORD i = VK_SPACE; i <= VK_DOWN; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// 分号、等号、逗号、-、句号、/、`
-			for (DWORD i = VK_OEM_1; i <= VK_OEM_3; ++i) {
-				keyCodes.insert(i);
-			}
-
-			// [、\、]、'
-			for (DWORD i = VK_OEM_4; i <= VK_OEM_7; ++i) {
-				keyCodes.insert(i);
-			}
-
-			keyCodes.insert(VK_INSERT);	// Insert
-			keyCodes.insert(VK_DELETE);	// Delete
-			keyCodes.insert(VK_ADD);		// 加（小键盘）
-			keyCodes.insert(VK_SUBTRACT);	// 减（小键盘）
-			keyCodes.insert(VK_MULTIPLY);	// 乘（小键盘）
-			keyCodes.insert(VK_DIVIDE);	// 除（小键盘）
-			keyCodes.insert(VK_DECIMAL);	// .（小键盘）
-			keyCodes.insert(VK_BACK);		// Backspace
-			keyCodes.insert(VK_RETURN);	// 回车
-
-			return keyCodes;
+		// 字母
+		for (DWORD i = 'A'; i <= 'Z'; ++i) {
+			keyCodes.insert(i);
 		}
+
+		// 数字（顶部）
+		for (DWORD i = '0'; i <= '9'; ++i) {
+			keyCodes.insert(i);
+		}
+
+		// 数字（小键盘）
+		for (DWORD i = VK_NUMPAD0; i <= VK_NUMPAD9; ++i) {
+			keyCodes.insert(i);
+		}
+
+		// F1~F24
+		for (DWORD i = VK_F1; i <= VK_F24; ++i) {
+			keyCodes.insert(i);
+		}
+
+		// 空格、Page Up/Down、End、Home、方向键
+		for (DWORD i = VK_SPACE; i <= VK_DOWN; ++i) {
+			keyCodes.insert(i);
+		}
+
+		// 分号、等号、逗号、-、句号、/、`
+		for (DWORD i = VK_OEM_1; i <= VK_OEM_3; ++i) {
+			keyCodes.insert(i);
+		}
+
+		// [、\、]、'
+		for (DWORD i = VK_OEM_4; i <= VK_OEM_7; ++i) {
+			keyCodes.insert(i);
+		}
+
+		keyCodes.insert(VK_INSERT);	// Insert
+		keyCodes.insert(VK_DELETE);	// Delete
+		keyCodes.insert(VK_ADD);		// 加（小键盘）
+		keyCodes.insert(VK_SUBTRACT);	// 减（小键盘）
+		keyCodes.insert(VK_MULTIPLY);	// 乘（小键盘）
+		keyCodes.insert(VK_DIVIDE);	// 除（小键盘）
+		keyCodes.insert(VK_DECIMAL);	// .（小键盘）
+		keyCodes.insert(VK_BACK);		// Backspace
+		keyCodes.insert(VK_RETURN);	// 回车
+
+		return keyCodes;
 	}();
 	
 	return result;
