a843f1e8e703a25bda9fb3ceed5fd62f42237f57
刘旭 <blinue@outlook.com>
2022-11-04 16:51:59 +0800

    test

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,7 +1,7 @@
-msbuild "/p:Configuration=Release;Platform=x64" "..\CONAN_INSTALL"
-msbuild "/p:Configuration=Release;Platform=x64;OutDir=..\..\publish\" "..\Effects"
-msbuild "/p:Configuration=Release;Platform=x64;OutDir=..\..\publish\" "..\Magpie.Core"
-msbuild "/m /p:Configuration=Release;Platform=x64;BuildProjectReferences=false;OutDir=..\..\publish\" "..\Magpie"
-msbuild "/p:Configuration=Release;Platform=x64;OutDir=..\..\publish\" "..\Updater"
+msbuild /p:Configuration=Release;Platform=x64 src\CONAN_INSTALL
+msbuild /p:Configuration=Release;Platform=x64;OutDir=publish src\Effects
+msbuild /p:Configuration=Release;Platform=x64;OutDir=publish src\Magpie.Core
+msbuild /m /p:Configuration=Release;Platform=x64;BuildProjectReferences=false;OutDir=publish src\Magpie
+msbuild /p:Configuration=Release;Platform=x64;OutDir=publish src\Updater
 
 COPY-ITEM -Path @("C:\Windows\System32\msvcp140.dll", "C:\Windows\System32\vcruntime140.dll", "C:\Windows\System32\vcruntime140_1.dll") -Destination "publish\"
