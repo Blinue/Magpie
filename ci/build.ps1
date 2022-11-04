568ee0c6a6bc8fda38f1639b165ad0dad3c65e2f
刘旭 <blinue@outlook.com>
2022-11-04 18:21:25 +0800

    test

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,10 +1,25 @@
 $ErrorActionPreference = "Stop"
 
 msbuild /p:Configuration=Release`;Platform=x64 src\CONAN_INSTALL
+if($LastExitCode -ne 0) {
+	throw '编译 CONAN_INSTALL 失败'
+}
 msbuild /p:Configuration=Release`;Platform=x64`;OutDir=..\..\publish\ src\Effects
+if($LastExitCode -ne 0) {
+	throw '编译 Effects 失败'
+}
 msbuild /p:Configuration=Release`;Platform=x64`;OutDir=..\..\publish\ src\Magpie.Core
+if($LastExitCode -ne 0) {
+	throw '编译 Magpie.Core 失败'
+}
 msbuild /m /p:Configuration=Release`;Platform=x64`;BuildProjectReferences=false`;OutDir=..\..\publish\ src\Magpie
+if($LastExitCode -ne 0) {
+	throw '编译 Magpie 失败'
+}
 msbuild /p:Configuration=Release`;Platform=x64`;OutDir=..\..\publish\ src\Updater
+if($LastExitCode -ne 0) {
+	throw '编译 Updater 失败'
+}
 
 cd .\publish\
 Remove-Item @("*.pdb", "*.lib", "*.exp", "*.winmd", "*.xml", "*.xbf", "dummy.*", "Microsoft.Web.WebView2.Core.dll")
