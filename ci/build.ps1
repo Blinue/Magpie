6919ccf0dcb4926489d8170fe22e2121b1dcb8a8
刘旭 <blinue@outlook.com>
2022-11-04 17:30:15 +0800

    test

--- ci/build.ps1
+++ ci/build.ps1
@@ -5,6 +5,7 @@ msbuild /m /p:Configuration=Release`;Platform=x64`;BuildProjectReferences=false`
 msbuild /p:Configuration=Release`;Platform=x64`;OutDir=..\..\publish\ src\Updater
 
 cd .\publish\
-Remove-Item ("*.pdb", "*.lib", "*.exp", "*.winmd", "*.xml", "*.xbf")
+Remove-Item @("*.pdb", "*.lib", "*.exp", "*.winmd", "*.xml", "*.xbf", "dummy.*", "Microsoft.Web.WebView2.Core.dll")
+Remove-Item @("Microsoft.UI.Xaml", "Magpie.UI") -Recurse
 
-COPY-ITEM -Path @("C:\Windows\System32\msvcp140.dll", "C:\Windows\System32\vcruntime140.dll", "C:\Windows\System32\vcruntime140_1.dll") -Destination "publish\"
+COPY-ITEM -Path @("C:\Windows\System32\msvcp140.dll", "C:\Windows\System32\vcruntime140.dll", "C:\Windows\System32\vcruntime140_1.dll")
