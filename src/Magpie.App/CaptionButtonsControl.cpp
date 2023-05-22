a3f277a2257a9e8b4755fc7fdf9d1798104dd92e
刘旭 <blinue@outlook.com>
2023-05-22 15:23:26 +0800

    feat: 添加 CaptionButtonsControl

---
+++ src/Magpie.App/CaptionButtonsControl.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "CaptionButtonsControl.h"
+#if __has_include("CaptionButtonsControl.g.cpp")
+#include "CaptionButtonsControl.g.cpp"
+#endif
+
+namespace winrt::Magpie::App::implementation {
+
+}
---
+++ src/Magpie.App/CaptionButtonsControl.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "CaptionButtonsControl.g.h"
+
+namespace winrt::Magpie::App::implementation {
+
+struct CaptionButtonsControl : CaptionButtonsControlT<CaptionButtonsControl> {
+    CaptionButtonsControl() {
+
+    }
+
+};
+
+}
+
+namespace winrt::Magpie::App::factory_implementation {
+
+struct CaptionButtonsControl : CaptionButtonsControlT<CaptionButtonsControl, implementation::CaptionButtonsControl> {
+};
+
+}
