28de6ea3bd70103eacd03ad71e87c9bb882154b8
Xu <34770031+Blinue@users.noreply.github.com>
2023-10-11 13:54:27 +0800

    Update publish.py

--- publish.py
+++ publish.py
@@ -8,13 +8,11 @@ import shutil
 from xml.etree import ElementTree
 
 try:
+    # https://docs.github.com/en/actions/learn-github-actions/variables
     if os.environ["GITHUB_ACTIONS"].lower() == "true":
         # 不知为何在 Github Actions 中运行时默认编码为 ANSI，而且每次 print 都刷新流才能正常显示
         for stream in [sys.stdout, sys.stderr]:
             stream.reconfigure(encoding='utf-8')
-        print("GITHUB_ACTIONS", flush=True)
-    else:
-        print("test", flush=True)
 except:
     pass
 
