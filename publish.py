75d431df59c91834fee5b68969016f632ed85af0
刘旭 <blinue@outlook.com>
2023-10-11 09:23:59 +0800

    test

--- publish.py
+++ publish.py
@@ -7,6 +7,7 @@ import glob
 import shutil
 from xml.etree import ElementTree
 
+# 不知为何在 Github Actions 中运行时默认编码为 ANSI，而且每次 print 都刷新流才能正常显示
 sys.stdout.reconfigure(encoding='utf-8')
 
 #####################################################################
@@ -40,8 +41,11 @@ def get_known_folder_path(folderid):
 
     SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
     SHGetKnownFolderPath.argtypes = [
-        ctypes.POINTER(GUID), wintypes.DWORD, wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
-    ] 
+        ctypes.POINTER(GUID),
+        wintypes.DWORD,
+        wintypes.HANDLE,
+        ctypes.POINTER(ctypes.c_wchar_p)
+    ]
 
     fid = GUID(folderid) 
     pPath = ctypes.c_wchar_p()
@@ -70,7 +74,7 @@ if not os.access(msbuildPath, os.X_OK):
 
 os.chdir(os.path.dirname(__file__))
 
-if os.system("\"" + msbuildPath + "\" /p:Configuration=Release;Platform=x64 src\\CONAN_INSTALL") != 0:
+if os.system("\"" + msbuildPath + "\" /p:Configuration=Release;Platform=x64 src\\CONAN_INSTALL") == 0:
     raise Exception("编译 CONAN_INSTALL 失败")
 
 if os.system("\"" + msbuildPath + "\" /p:Configuration=Release;Platform=x64;OutDir=..\\..\\publish\\ src\\Effects") != 0:
