e9598aa26d25e43bdc7e36831ccb97eb67da81a2
刘旭 <34770031+Blinue@users.noreply.github.com>
2023-10-13 23:03:49 +0800

    Revert "chore: 使用 MSBuild 自动编译 Conan 依赖"
    This reverts commit b5531986acfac5c4fbe734937b9dfcfb5b7fc08b.

--- src/CONAN_INSTALL/ConanInstall.bat
+++ src/CONAN_INSTALL/ConanInstall.bat
@@ -1,8 +1,30 @@
-cd ..\Magpie.Core
-python ..\conan_install.py %2 %1
+conan --version
 
-cd ..\Magpie.App
-python ..\conan_install.py %2 %1
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO Error: Conan not found
+	EXIT 1
+)
 
-cd ..\Magpie
-python ..\conan_install.py %2 %1
+conan config set storage.path=%CD%\..\..\.conan\data
+
+IF %1 == Debug (
+	IF %2 == x64 (
+		conan install ..\Magpie\conanfile.txt --install-folder ..\..\.conan\x64\Debug\Magpie --build=outdated -s build_type=Debug -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MTd --update
+		conan install ..\Magpie.Core\conanfile.txt --install-folder ..\..\.conan\x64\Debug\Magpie.Core --build=outdated -s build_type=Debug -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MTd --update
+		conan install ..\Magpie.App\conanfile.txt --install-folder ..\..\.conan\x64\Debug\Magpie.App --build=outdated -s build_type=Debug -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MTd --update
+	) ELSE (
+		conan install ..\Magpie\conanfile.txt --install-folder ..\..\.conan\ARM64\Debug\Magpie --build=outdated -s build_type=Debug -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MTd --update
+		conan install ..\Magpie.Core\conanfile.txt --install-folder ..\..\.conan\ARM64\Debug\Magpie.Core --build=outdated -s build_type=Debug -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MTd --update
+		conan install ..\Magpie.App\conanfile.txt --install-folder ..\..\.conan\ARM64\Debug\Magpie.App --build=outdated -s build_type=Debug -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MTd --update
+	)
+) ELSE (
+	IF %2 == x64 (
+		conan install ..\Magpie\conanfile.txt --install-folder ..\..\.conan\x64\Release\Magpie --build=outdated -s build_type=Release -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MT --update
+		conan install ..\Magpie.Core\conanfile.txt --install-folder ..\..\.conan\x64\Release\Magpie.Core --build=outdated -s build_type=Release -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MT --update
+		conan install ..\Magpie.App\conanfile.txt --install-folder ..\..\.conan\x64\Release\Magpie.App --build=outdated -s build_type=Release -s arch=x86_64 -s compiler.version=17 -s compiler.runtime=MT --update
+	) ELSE (
+		conan install ..\Magpie\conanfile.txt --install-folder ..\..\.conan\ARM64\Release\Magpie --build=outdated -s build_type=Release -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MT --update
+		conan install ..\Magpie.Core\conanfile.txt --install-folder ..\..\.conan\ARM64\Release\Magpie.Core --build=outdated -s build_type=Release -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MT --update
+		conan install ..\Magpie.App\conanfile.txt --install-folder ..\..\.conan\ARM64\Release\Magpie.App --build=outdated -s build_type=Release -s arch=armv8 -s compiler.version=17 -s compiler.runtime=MT --update
+	)
+)
