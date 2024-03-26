1de35ab9da72c6f9bdb8e1a3f662dfe9919fa10e
Xu <34770031+Blinue@users.noreply.github.com>
2024-03-26 19:27:58 +0800

    简化 ScalingRuntime 状态 (#862)
    * refactor: 简化 ScalingRuntime
    
    * fix: 修复特定情况下退出时死锁的问题

--- src/Magpie.Core/ScalingRuntime.cpp
+++ src/Magpie.Core/ScalingRuntime.cpp
@@ -6,32 +6,62 @@
 
 namespace Magpie::Core {
 
-ScalingRuntime::ScalingRuntime() : _scalingThread(std::bind(&ScalingRuntime::_ScalingThreadProc, this)) {
+ScalingRuntime::ScalingRuntime() :
+	_scalingThread(std::bind(&ScalingRuntime::_ScalingThreadProc, this)) {
 }
 
 ScalingRuntime::~ScalingRuntime() {
 	Stop();
 
 	if (_scalingThread.joinable()) {
-		const DWORD magWndThreadId = GetThreadId(_scalingThread.native_handle());
-		// 持续尝试直到 _scalingThread 创建了消息队列
-		while (!PostThreadMessage(magWndThreadId, WM_QUIT, 0, 0)) {
-			Sleep(0);
+		const HANDLE hScalingThread = _scalingThread.native_handle();
+
+		{
+			const DWORD magWndThreadId = GetThreadId(hScalingThread);
+			// 持续尝试直到 _scalingThread 创建了消息队列
+			while (!PostThreadMessage(magWndThreadId, WM_QUIT, 0, 0)) {
+				Sleep(0);
+			}
 		}
+
+		// 等待缩放线程退出，在此期间必须处理消息队列，否则缩放线程调用
+		// SetWindowLongPtr 会导致死锁
+		while (true) {
+			MSG msg;
+			while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+			if (MsgWaitForMultipleObjectsEx(1, &hScalingThread,
+				INFINITE, QS_ALLINPUT, MWMO_INPUTAVAILABLE) == WAIT_OBJECT_0) {
+				// WAIT_OBJECT_0 表示缩放线程已退出
+				// WAIT_OBJECT_0 + 1 表示有新消息
+				break;
+			}
+		}
+		
 		_scalingThread.join();
 	}
 }
 
 void ScalingRuntime::Start(HWND hwndSrc, ScalingOptions&& options) {
-	HWND expected = NULL;
-	if (!_hwndSrc.compare_exchange_strong(expected, hwndSrc, std::memory_order_relaxed)) {
+	_State expected = _State::Idle;
+	if (!_state.compare_exchange_strong(
+		expected, _State::Initializing, std::memory_order_relaxed)) {
 		return;
 	}
 
 	_isRunningChangedEvent(true);
 
-	_Dispatcher().TryEnqueue([dispatcher(_Dispatcher()), hwndSrc, options(std::move(options))]() mutable {
-		ScalingWindow::Get().Create(dispatcher, hwndSrc, std::move(options));
+	_Dispatcher().TryEnqueue([this, dispatcher(_Dispatcher()), hwndSrc, options(std::move(options))]() mutable {
+		if (ScalingWindow::Get().Create(dispatcher, hwndSrc, std::move(options))) {
+			_state.store(_State::Scaling, std::memory_order_relaxed);
+		} else {
+			// 缩放失败
+			_state.store(_State::Idle, std::memory_order_relaxed);
+			_isRunningChangedEvent(false);
+		}
 	});
 }
 
@@ -53,6 +83,7 @@ void ScalingRuntime::Stop() {
 	}
 
 	_Dispatcher().TryEnqueue([]() {
+		// 消息循环会更改 _state
 		ScalingWindow& scalingWindow = ScalingWindow::Get();
 		if (scalingWindow.IsSrcRepositioning()) {
 			scalingWindow.CleanAfterSrcRepositioned();
@@ -123,8 +154,8 @@ void ScalingRuntime::_ScalingThreadProc() noexcept {
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			if (msg.message == WM_QUIT) {
 				scalingWindow.Destroy();
-				
-				if (_hwndSrc.exchange(NULL, std::memory_order_relaxed)) {
+
+				if (_state.exchange(_State::Idle, std::memory_order_relaxed) != _State::Idle) {
 					_isRunningChangedEvent(false);
 				}
 
@@ -134,12 +165,17 @@ void ScalingRuntime::_ScalingThreadProc() noexcept {
 			DispatchMessage(&msg);
 		}
 
+		if (_state.load(std::memory_order_relaxed) != _State::Scaling) {
+			WaitMessage();
+			continue;
+		}
+
 		if (scalingWindow) {
 			scalingWindow.Render();
 			MsgWaitForMultipleObjectsEx(0, nullptr, 1, QS_ALLINPUT, MWMO_INPUTAVAILABLE);
 		} else if (scalingWindow.IsSrcRepositioning()) {
 			const int state = GetSrcRepositionState(
-				_hwndSrc.load(std::memory_order_relaxed),
+				scalingWindow.HwndSrc(),
 				scalingWindow.Options().IsAllowScalingMaximized()
 			);
 			if (state == 0) {
@@ -153,11 +189,9 @@ void ScalingRuntime::_ScalingThreadProc() noexcept {
 				ScalingWindow::Get().CleanAfterSrcRepositioned();
 			}
 		} else {
-			if (_hwndSrc.exchange(NULL, std::memory_order_relaxed)) {
-				// 缩放失败或立即退出缩放
-				_isRunningChangedEvent(false);
-			}
-			WaitMessage();
+			// 缩放结束
+			_state.store(_State::Idle, std::memory_order_relaxed);
+			_isRunningChangedEvent(false);
 		}
 	}
 }
