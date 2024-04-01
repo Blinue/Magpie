85c489a189031a4c4e69f0ee26cfc485d6f9665b
刘旭 <blinue@outlook.com>
2024-04-01 15:33:52 +0800

    feat: 自行实现 SourceLocation，没有冗长的函数名

--- src/Shared/Logger.cpp
+++ src/Shared/Logger.cpp
@@ -9,7 +9,7 @@ bool Logger::Initialize(spdlog::level::level_enum logLevel, const char* logFileN
 	try {
 		_logger = spdlog::rotating_logger_mt(".", logFileName, logArchiveAboveSize, logMaxArchiveFiles);
 		_logger->set_level(logLevel);
-		_logger->set_pattern("%Y-%m-%d %H:%M:%S.%e|%l|%s:%!|%v");
+		_logger->set_pattern("%Y-%m-%d %H:%M:%S.%e|%l|%s:%#|%!|%v");
 		_logger->flush_on(spdlog::level::warn);
 		spdlog::flush_every(5s);
 	} catch (const spdlog::spdlog_ex&) {
@@ -44,7 +44,7 @@ std::string Logger::_MakeComErrorMsg(std::string_view msg, HRESULT hr) {
 	return fmt::sprintf("%s\n\tHRESULT：0x%X", msg, hr);
 }
 
-void Logger::_Log(spdlog::level::level_enum logLevel, std::string_view msg, const std::source_location& location) {
+void Logger::_Log(spdlog::level::level_enum logLevel, std::string_view msg, const SourceLocation& location) {
 	assert(!msg.empty());
 
 	if (logLevel >= spdlog::level::warn) {
@@ -57,7 +57,7 @@ void Logger::_Log(spdlog::level::level_enum logLevel, std::string_view msg, cons
 	}
 
 	_logger->log(
-		spdlog::source_loc{ location.file_name(), (int)location.line(), location.function_name() },
+		spdlog::source_loc{ location.FileName(), (int)location.Line(), location.FunctionName() },
 		logLevel,
 		msg
 	);
