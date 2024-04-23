7d5b42571fc66c7aa5b22afd0fc1d305d41d1e0d
刘旭 <blinue@outlook.com>
2024-04-23 15:31:59 +0800

    feat: 添加 TouchHelper 项目

---
+++ src/TouchHelper/main.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+int APIENTRY wWinMain(
+	_In_ HINSTANCE /*hInstance*/,
+	_In_opt_ HINSTANCE /*hPrevInstance*/,
+	_In_ LPWSTR /*lpCmdLine*/,
+	_In_ int /*nCmdShow*/
+) {
+	return 0;
+}
