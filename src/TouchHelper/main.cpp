ea5eeaf108a369a93c0884581bae2717d78b729a
刘旭 <blinue@outlook.com>
2024-04-24 13:38:25 +0800

    feat: 添加自签名证书

--- src/TouchHelper/main.cpp
+++ src/TouchHelper/main.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include <magnification.h>
 
 int APIENTRY wWinMain(
 	_In_ HINSTANCE /*hInstance*/,
@@ -6,5 +7,17 @@ int APIENTRY wWinMain(
 	_In_ LPWSTR /*lpCmdLine*/,
 	_In_ int /*nCmdShow*/
 ) {
+	if (!MagInitialize()) {
+		MessageBox(NULL, L"失败", L"消息", MB_OK);
+		return 1;
+	}
+
+	RECT srcRect{ 0,0,100,100 };
+	RECT destRect{ 10,10,210,210 };
+	if (!MagSetInputTransform(TRUE, &srcRect, &destRect)) {
+		MessageBox(NULL, L"失败", L"消息", MB_OK);
+		return 1;
+	}
+	
 	return 0;
 }
