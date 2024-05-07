2b733859dc0081e6ca183d8de2fb31e5da7ae471
Xu <34770031+Blinue@users.noreply.github.com>
2024-05-07 22:41:05 +0800

    UI: 优化触控支持 UI

---
+++ src/Magpie.App/TouchHelper.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "TouchHelper.h"
+
+namespace winrt::Magpie::App {
+
+bool TouchHelper::IsTouchSupportEnabled() noexcept {
+	return false;
+}
+
+void TouchHelper::IsTouchSupportEnabled(bool value) noexcept {
+}
+
+}
---
+++ src/Magpie.App/TouchHelper.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace winrt::Magpie::App {
+
+struct TouchHelper {
+	static bool IsTouchSupportEnabled() noexcept;
+	static void IsTouchSupportEnabled(bool value) noexcept;
+};
+
+}
