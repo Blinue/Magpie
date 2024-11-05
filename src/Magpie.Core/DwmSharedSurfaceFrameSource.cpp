045e4a51d198d54dc4d73703da471781bf8d6081
Xu <xu.liu@bonsmile.com>
2024-11-05 13:15:41 +0800

    feat: 如果窗口没有重定向表面，GDI 和 DwmSharedSurface 捕获直接失败

--- src/Magpie.Core/DwmSharedSurfaceFrameSource.cpp
+++ src/Magpie.Core/DwmSharedSurfaceFrameSource.cpp
@@ -38,32 +38,21 @@ bool DwmSharedSurfaceFrameSource::_Initialize() noexcept {
 	HWND hwndSrc = ScalingWindow::Get().HwndSrc();
 
 	RECT frameRect;
-	if (double a, bx, by; _GetMapToOriginDPI(hwndSrc, a, bx, by)) {
-		Logger::Get().Info(fmt::format("源窗口 DPI 缩放为 {}", 1 / a));
-
-		frameRect = RECT{
-			std::lround(_srcRect.left * a + bx),
-			std::lround(_srcRect.top * a + by),
-			std::lround(_srcRect.right * a + bx),
-			std::lround(_srcRect.bottom * a + by)
-		};
-	} else {
+	double a, bx, by;
+	if (!_GetMapToOriginDPI(hwndSrc, a, bx, by)) {
+		// 很可能是因为窗口没有重定向表面，这种情况下 DwmSharedSurface 捕获肯定失败
 		Logger::Get().Error("_GetMapToOriginDPI 失败");
-
-		// _GetMapToOriginDPI 失败则假设 DPI 缩放为 1
-		RECT srcWindowRect;
-		if (!GetWindowRect(hwndSrc, &srcWindowRect)) {
-			Logger::Get().Win32Error("GetWindowRect 失败");
-			return false;
-		}
-
-		frameRect = RECT{
-			_srcRect.left - srcWindowRect.left,
-			_srcRect.top - srcWindowRect.top,
-			_srcRect.right - srcWindowRect.left,
-			_srcRect.bottom - srcWindowRect.top
-		};
+		return false;
 	}
+
+	Logger::Get().Info(fmt::format("源窗口 DPI 缩放为 {}", 1 / a));
+
+	frameRect = RECT{
+		std::lround(_srcRect.left * a + bx),
+		std::lround(_srcRect.top * a + by),
+		std::lround(_srcRect.right * a + bx),
+		std::lround(_srcRect.bottom * a + by)
+	};
 	
 	if (frameRect.left < 0 || frameRect.top < 0 || frameRect.right < 0
 		|| frameRect.bottom < 0 || frameRect.right - frameRect.left <= 0
