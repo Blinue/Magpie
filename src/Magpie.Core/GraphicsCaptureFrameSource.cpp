65a92fe189b25fe9761bbdd7a8472c31fa33a615
Xu <blinue@outlook.com>
2025-04-05 19:42:28 +0800

    chore: 最低 26100 SDK

--- src/Magpie.Core/GraphicsCaptureFrameSource.cpp
+++ src/Magpie.Core/GraphicsCaptureFrameSource.cpp
@@ -422,7 +422,6 @@ bool GraphicsCaptureFrameSource::_StartCapture() noexcept {
 			winrt::name_of<winrt::GraphicsCaptureSession>(),
 			L"IsCursorCaptureEnabled"
 		)) {
-			// 
 			_captureSession.IsCursorCaptureEnabled(false);
 		}
 
