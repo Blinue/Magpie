19a43adccf6b33d2bcd4c23b4a418504630c58ed
Xu <blinue@outlook.com>
2025-08-26 19:26:17 +0800

    fix: 增加两个窗口位于同一进程的条件

--- src/Magpie/ScalingService.cpp
+++ src/Magpie/ScalingService.cpp
@@ -208,19 +208,38 @@ static void ShowError(HWND hWnd, ScalingError error) noexcept {
 	Logger::Get().Error(fmt::format("缩放失败\n\t错误码: {}", (int)error));
 }
 
-static bool IsOwnedWindow(HWND hwndTest, HWND hwndOwner) noexcept {
-	HWND hwndCur = hwndTest;
-	while (bool(hwndCur = GetWindowOwner(hwndCur))) {
-		if (hwndCur == hwndOwner) {
-			return true;
+static bool IsPopupWindow(HWND hwndPopup, HWND hwndOwner) noexcept {
+	// 检查所有者关系
+	{
+		HWND hwndCur = hwndPopup;
+		while (bool(hwndCur = GetWindowOwner(hwndCur))) {
+			if (hwndCur == hwndOwner) {
+				OutputDebugString(L"\n所有|");
+				return true;
+			}
 		}
 	}
-	return false;
-}
+	
+	// 有些游戏不用所有者关系来实现弹窗，而是将主窗口禁用，做到和模态弹窗差不多的效果。
+	// 这不可能准确检测，只能尽可能增加限制以减少误判，我们检查三个条件：
+	// 
+	// 1. 主窗口处于禁用状态
+	// 2. 两个窗口位于同一个进程
+	// 3. 主窗口没有传统意义的弹窗
 
-static bool IsPopupWindow(HWND hwndPopup, HWND hwndOwner) noexcept {
-	return IsOwnedWindow(hwndPopup, hwndOwner) ||
-		(!IsWindowEnabled(hwndOwner) && !GetWindow(hwndOwner, GW_ENABLEDPOPUP));
+	if (IsWindowEnabled(hwndOwner)) {
+		return false;
+	}
+
+	DWORD pid1 = 0;
+	DWORD pid2 = 0;
+	GetWindowThreadProcessId(hwndPopup, &pid1);
+	GetWindowThreadProcessId(hwndOwner, &pid2);
+	if (pid1 != pid2) {
+		return false;
+	}
+
+	return !GetWindow(hwndOwner, GW_ENABLEDPOPUP);
 }
 
 static bool IsReadyForScaling(HWND hwndFore) noexcept {
