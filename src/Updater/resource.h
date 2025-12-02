7e4be98b5bd1382597d04b9155f19a6afbf040c6
Xu <xu.liu@bonsmile.com>
2025-12-02 14:29:02 +0800

    chore: 简化 resource.h

--- src/Updater/resource.h
+++ src/Updater/resource.h
@@ -1,17 +1,3 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ 生成的包含文件。
-// 供 Updater.rc 使用
-//
-#define IDI_UPDATER                     101
+#pragma once
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDI_UPDATER 101
